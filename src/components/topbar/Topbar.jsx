5fcdcd72afb4b42534bba375eb68c4235b9cf162
Naman Sogani <nmnsgn05@gmail.com>
2023-02-01 23:38:38 +0530

    First Commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './topbar.scss'
+import {Person, Mail, BusinessCenter, LinkedIn, GitHub} from "@material-ui/icons";
+import { Button } from '@material-ui/core';
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch('Naman Sogani_10690967.pdf').then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = "Naman's Resume.pdf";
+            alink.click();
+        })
+    })
+}
+
+  return (
+    <div className={'topbar ' + (menuOpen && "active")}>
+        <div className="wrapper">
+            <div className='left'>
+                <a href='#intro' className='logo'><BusinessCenter className='briefcase' /></a>
+                <div className='itemContainer'>
+                  <Person className='icon' />
+                  <span>+91 7822 064 502</span>
+                </div>
+
+                <div className='itemContainer'>
+                  <Mail className='icon' />
+                  <span>nmnsgn@gmail.com</span>
+                </div>
+
+                <div className='itemContainer'>
+                <a href="https://www.linkedin.com/in/naman-sogani/">
+                  <LinkedIn className='icon' />
+                  </a>
+                </div>
+                <div className='itemContainer'>
+                <a href="https://github.com/Naman-Sogani">
+                  <GitHub className='icon' />
+                  </a>
+                </div>
+
+
+                <div  className = 'itemContainer '>
+                <span>
+                <Button variant="outlined" className='buttonContainer' onClick={onButtonClick} >Resume</Button>
+                </span>
+                </div>
+            </div>
+            <div className='right'>
+            <div className='hamburger' onClick={()=>setMenuOpen(!menuOpen)}>
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
+            </div>
+        </div>     
+    </div>
+  )
+}
---
+++ src/components/works/Works.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import "./works.scss";
+
+export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Snake Game",
+      desc:
+        "A snake game is an arcade game that involves a snake moving inside a box. Your score increases based on how many objects/fruit the snake eats. If it collides with itself or to the boundary of the box then the game is over.",
+      img:
+        "./apps/snakegame.PNG",
+      link: "https://appsnakegame.netlify.app",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Movie App",
+      desc:
+        "The movie app is basically a project focuses on the web Api calls. Here the user needs to search a movie and based on the keyword entered",
+      img:
+        "./apps/movieapp.PNG",
+        link: "https://movieappwebapi.netlify.app",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+        link: "https://appsnakegame.netlify.app",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span><a href={d.link} target="_blank">Project</a></span>
+                </div>
+              </div>
+              <div className="right">
+                <img
+                  // src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  src={d.img}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
+}
